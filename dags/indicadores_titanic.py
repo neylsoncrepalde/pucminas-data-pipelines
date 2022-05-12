abc9ecba0e2cba03d11ac7412bd723d7a0a30b4a
Neylson Crepalde <neylson.crepalde@a3data.com.br>
2022-05-11 22:11:53 -0300

    corrige import Variables

--- dags/indicadores_titanic.py
+++ dags/indicadores_titanic.py
@@ -1,7 +1,7 @@
 from airflow.decorators import task, dag
 from airflow.operators.python import PythonOperator
 from airflow.operators.dummy import DummyOperator
-from airflow.utils.variables import Variable
+from airflow.models import Variable
 from datetime import datetime
 import boto3
 
